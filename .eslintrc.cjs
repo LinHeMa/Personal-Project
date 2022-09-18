23817298a2113080e458092b2e81a40c730389d0
linHeMa <linhema0921@gmail.com>
2022-09-18 08:36:56 +0800

    fix eslintrc modules problem

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,10 @@
 module.exports = {
+  // shoud add env property for telling ESLint you are in Node environment.
+  env: {
+    browser: true,
+    amd: true,
+    node: true
+  },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
